0d18b48ea877ca5286ae1374c066b78bdf3a9e14
ebattat <ebattat@redhat.com>
2021-08-18 19:05:26 +0300

    fix undeply issue and azure cluster loading time

--- benchmark_runner/benchmark_operator/benchmark_operator_workloads.py
+++ benchmark_runner/benchmark_operator/benchmark_operator_workloads.py
@@ -131,7 +131,7 @@ class BenchmarkOperatorWorkloads:
         benchmark_operator_path = 'benchmark-operator'
         current_dir = os.getcwd()
         os.chdir(os.path.join(runner_path, benchmark_operator_path))
-        # @TODO delete this walk around for PR https://github.com/cloud-bulldozer/benchmark-operator/pull/647
+        # @TODO delete this walk around after fixing PR https://github.com/cloud-bulldozer/benchmark-operator/pull/647
         self.__ssh.run('make kustomize')
         self.__ssh.run('make undeploy')
         self.__oc.wait_for_pod_terminate(pod_name='benchmark-controller-manager')
