cf3580fff5b44c8f68182bf75956993ccf0b9bde
ebattat <ebattat@redhat.com>
2021-11-04 20:21:32 +0200

    update db memory

--- benchmark_runner/main/environment_variables.py
+++ benchmark_runner/main/environment_variables.py
@@ -112,7 +112,7 @@ class EnvironmentVariables:
         # remote ssh timeout - 3 hours for installation time
         self._environment_variables_dict['provision_timeout'] = os.environ.get(f'{self.__ocp_env_flavor}_PROVISION_TIMEOUT', '10800')
         # General timeout - 1.5 hours wait for pod/vm/upload data to elasticsearch
-        self._environment_variables_dict['timeout'] = os.environ.get(f'{self.__ocp_env_flavor}_TIMEOUT', '5400')
+        self._environment_variables_dict['timeout'] = os.environ.get(f'{self.__ocp_env_flavor}_TIMEOUT', '3600')
 
         # Parameters below related to 'update_ci_status()'
         # CI run time
