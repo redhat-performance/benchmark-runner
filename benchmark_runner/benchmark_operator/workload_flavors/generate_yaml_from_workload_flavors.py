e26136aa6041b2f04f56630c7f0d3fc781f4ac15
Robert Krawitz <rkrawitz@redhat.com>
2021-11-04 15:40:22 -0400

    First round of template simplification: separate generation of data files from actual creation

--- benchmark_runner/benchmark_operator/workload_flavors/generate_yaml_from_workload_flavors.py
+++ benchmark_runner/benchmark_operator/workload_flavors/generate_yaml_from_workload_flavors.py
@@ -3,17 +3,21 @@ import os
 import shutil
 import yaml
 from jinja2 import Template
-from benchmark_runner.main.update_data_template_yaml_with_environment_variables import delete_generate_file, update_environment_variable
+from benchmark_runner.main.update_data_template_yaml_with_environment_variables import render_yaml_file_with_environment
 from benchmark_runner.common.logger.logger_time_stamp import logger_time_stamp
 from benchmark_runner.main.environment_variables import environment_variables
 
 
 class TemplateOperations:
-    """This class is responsible for template operations"""
+    """
+    This class is responsible for template operations
+    
+    """
 
     def __init__(self):
-        # environment variables
-        self.__environment_variables_dict = environment_variables.environment_variables_dict
+        self.initialize_environment(environment_variables.environment_variables_dict)
+
+    def __initialize_dependent_variables__(self):
         self.__run_type = self.__environment_variables_dict.get('run_type', '')
         self.__dir_path = f'{os.path.dirname(os.path.realpath(__file__))}/{self.__run_type}'
         self.__current_run_path = f'{self.__dir_path}/current_run'
@@ -25,6 +29,23 @@ class TemplateOperations:
         else:
             self.__storage_type = 'ephemeral'
 
+    def initialize_environment(self, environment: dict = environment_variables.environment_variables_dict):
+        self.__environment_variables_dict = environment
+        self.__initialize_dependent_variables__()
+
+    def set_environment_variable(self, variable: str, value: str):
+        self.__environment_variables_dict[variable] = value
+        self.__initialize_dependent_variables__()
+
+    def set_environment_variables(self, update_environment: dict):
+        self.__environment_variables_dict = {**self.__environment_variables_dict, **update_environment}
+        self.__initialize_dependent_variables__()
+
+    def clear_environment_variable(self, variable):
+        self.__environment_variables_dict.pop(variable, '')
+        self.__initialize_dependent_variables__()
+
+    @logger_time_stamp
     def __get_yaml_template_by_workload(self, workload: str, extension='.yaml', skip: str = 'data'):
         """
         This method return yaml names in benchmark_operator folder
@@ -40,69 +61,89 @@ class TemplateOperations:
                     return os.path.splitext(file)[0]
 
     @logger_time_stamp
-    def generate_hammerdb_yamls(self, workload: str, database: str):
+    def generate_files(self, data_files: dict):
+        for filename, data in data_files.items():
+            with open(os.path.join(self.__current_run_path, filename), 'w') as f:
+                f.write(data)
+
+    def __get_workload_keys(self, workload: str):
         """
-        This method generate hammerdb yaml from workload_flavors,
-        special generator for 2 yaml file: database and workload
-        :return:
+        Compute kind and data key from workload name
         """
-        # replace environment variables and generate hammerdb_data.yaml
-        if 'kata' in workload:
-            environment_variables.environment_variables_dict['kind'] = 'kata'
+        if '_pod' in workload:
+            return 'pod', 'pod'
+        elif '_kata' in workload:
+            return 'kata', 'pod'
+        elif '_vm' in workload:
+            return 'vm', 'vm'
         else:
-            environment_variables.environment_variables_dict['kind'] = 'pod'
-        update_environment_variable(dir_path=self.__hammerdb_dir_path, yaml_file='hammerdb_data_template.yaml')
-        # handle database pod yaml
-        if 'pod' in workload or 'kata' in workload:
-            # replace environment variables
-            update_environment_variable(dir_path=self.__hammerdb__internal_dir_path, yaml_file=f'{database}_{self.__storage_type}_template.yaml')
-            shutil.move(os.path.join(self.__hammerdb__internal_dir_path, f'{database}_{self.__storage_type}.yaml'), os.path.join(self.__current_run_path, f'{database}.yaml'))
-            delete_generate_file(full_path_yaml=os.path.join(self.__hammerdb__internal_dir_path, f'{database}.yaml'))
+            return None, None
+
+    @logger_time_stamp
+    def generate_hammerdb_yamls_internal(self, workload: str, database: str):
+        """
+        Generate required .yaml files as a dictionary of file names and contents
+        :return: Dictionary <filename:contents>
+        """
         # Get hammerdb data
-        with open(os.path.join(self.__hammerdb_dir_path, 'hammerdb_data.yaml'), 'r') as file:
-            hammerdb_data = yaml.load(file, Loader=yaml.FullLoader)
-        shared_data = hammerdb_data['shared_data']
-        shared_data_pod = hammerdb_data['pod']
-        shared_data_vm = hammerdb_data['vm']
+        hammerdb_template = self.__get_yaml_template_by_workload(workload=workload)
+        hammerdb_data = yaml.load(render_yaml_file_with_environment(self.__hammerdb_dir_path, 'hammerdb_data_template.yaml', self.__environment_variables_dict), Loader=yaml.FullLoader)
         database_data = hammerdb_data[database]
+        render_data = hammerdb_data['shared_data']
+
+        # merge 3 dictionaries
+        kind, template_kind = self.__get_workload_keys(workload)
+        render_data['kind'] = kind
+        render_data = {**self.__environment_variables_dict, **render_data, **database_data, **hammerdb_data[template_kind]}
 
         # Jinja render hammerdb yaml
-        hammerdb_template = self.__get_yaml_template_by_workload(workload=workload)
         with open(os.path.join(self.__hammerdb_dir_path, 'internal_data', f'{hammerdb_template}.yaml')) as f:
-            template_str = f.read()
-        tm = Template(template_str)
-        # merge 3 dictionaries
-        if 'vm' in hammerdb_template:
-            shared_data = {**shared_data, **shared_data_vm}
-            render_data = {**shared_data, **database_data}
-            render_data['kind'] = 'vm'
-        elif 'pod' in hammerdb_template:
-            shared_data = {**shared_data, **shared_data_pod}
-            render_data = {**shared_data, **database_data}
-            if 'kata' in workload:
-                render_data['kind'] = 'kata'
-            else:
-                render_data['kind'] = 'pod'
-
-        data = tm.render(render_data)
+            tm = Template(f.read())
         hammerdb_name = hammerdb_template.replace('template', '')
-        with open(os.path.join(f'{self.__current_run_path}', f'{hammerdb_name}{database}.yaml'), 'w') as f:
-            f.write(data)
+        answer = {}
+        answer[f'{hammerdb_name}{database}.yaml'] = tm.render(render_data)
 
         # Jinja render database pod yaml
         if 'pod' in workload or 'kata' in workload:
             # replace parameter from hammerdb_data
             database_name = f'{database}_{self.__storage_type}_template.yaml'
             with open(os.path.join(self.__hammerdb__internal_dir_path, database_name)) as f:
-                database_template_str = f.read()
-            database_tm = Template(database_template_str)
-            database_data = database_tm.render(render_data)
-            with open(os.path.join(f'{self.__current_run_path}', f'{database}.yaml'), 'w') as f:
-                f.write(database_data)
+                database_template = Template(f.read())
+            answer[f'{database}.yaml'] = database_template.render(render_data)
 
-        # delete the generate data file with environment variable
-        delete_generate_file(os.path.join(self.__hammerdb_dir_path, 'hammerdb_data.yaml'))
-        # removing current_run yaml folder is occurred at the end of run: BenchmarkOperatorWorkloads__remove_run_workload_yaml_file
+        return answer
+
+    @logger_time_stamp
+    def generate_hammerdb_yamls(self, workload: str, database: str):
+        """
+        This method generate hammerdb yaml from workload_flavors,
+        special generator for 2 yaml file: database and workload
+        :return:
+        """
+        self.generate_files(self.generate_hammerdb_yamls_internal(workload=workload, database=database))
+
+    @logger_time_stamp
+    def generate_workload_yamls_internal(self, workload: str):
+        """
+        This method generate workload yaml from template
+        :return: Dictionary <filename:contents>
+        """
+
+        workload_template = self.__get_yaml_template_by_workload(workload=workload)
+        workload_name = workload.split('_')[0]
+        workload_dir_path = os.path.join(self.__dir_path, workload_name)
+        workload_data = yaml.load(render_yaml_file_with_environment(workload_dir_path, f'{workload_name}_data_template.yaml', self.__environment_variables_dict), Loader=yaml.FullLoader)
+        render_data = workload_data['shared_data']
+
+        # merge 3 dictionaries
+        kind, template_kind = self.__get_workload_keys(workload)
+        render_data['kind'] = kind
+        render_data = {**self.__environment_variables_dict, **render_data, **workload_data[template_kind]}
+
+        with open(os.path.join(f'{workload_dir_path}', 'internal_data', f'{workload_template}.yaml')) as f:
+            tm = Template(f.read())
+        workload_file_name = workload_template.replace('_template', '')
+        return {f'{workload_file_name}.yaml': tm.render(render_data)}
 
     @logger_time_stamp
     def generate_workload_yamls(self, workload: str):
@@ -110,42 +151,4 @@ class TemplateOperations:
         This method generate workload yaml from template
         :return:
         """
-
-        # Get workload data
-        if 'kata' in workload:
-            environment_variables.environment_variables_dict['kind'] = 'kata'
-        else:
-            environment_variables.environment_variables_dict['kind'] = 'pod'
-        workload_name = workload.split('_')[0]
-        workload_dir_path = os.path.join(self.__dir_path, workload_name)
-        update_environment_variable(dir_path=workload_dir_path, yaml_file=f'{workload_name}_data_template.yaml')
-        with open(os.path.join(workload_dir_path, f'{workload_name}_data.yaml'), 'r') as file:
-            workload_data = yaml.load(file, Loader=yaml.FullLoader)
-        shared_data = workload_data['shared_data']
-        shared_data_pod = workload_data['pod']
-        shared_data_vm = workload_data['vm']
-
-        workload_template = self.__get_yaml_template_by_workload(workload=workload)
-        template_file_path = os.path.join(f'{workload_dir_path}', 'internal_data', f'{workload_template}.yaml')
-        with open(template_file_path) as f:
-            template_str = f.read()
-        tm = Template(template_str)
-
-        # merge 3 dictionaries
-        if 'vm' in workload_template:
-            render_data = {**shared_data, **shared_data_vm}
-            render_data['kind'] = 'vm'
-        elif 'pod' in workload_template:
-            render_data = {**shared_data, **shared_data_pod}
-            if 'kata' in workload:
-                render_data['kind'] = 'kata'
-            else:
-                render_data['kind'] = 'pod'
-
-        data = tm.render(render_data)
-        workload_file_name = workload_template.replace('_template', '')
-        with open(os.path.join(f'{self.__current_run_path}', f'{workload_file_name}.yaml'), 'w') as f:
-            f.write(data)
-        # delete the generate data file with environment variable
-        delete_generate_file(os.path.join(workload_dir_path, f'{workload_name}_data.yaml'))
-        # removing current_run yaml folder is occurred at the end of run: BenchmarkOperatorWorkloads__remove_run_workload_yaml_file
+        self.generate_files(self.generate_workload_yamls_internal(workload=workload))
--- benchmark_runner/main/update_data_template_yaml_with_environment_variables.py
+++ benchmark_runner/main/update_data_template_yaml_with_environment_variables.py
@@ -5,6 +5,35 @@ from benchmark_runner.main.environment_variables import environment_variables
 from benchmark_runner.common.oc.oc import OC
 
 
+def render_yaml_string_with_environment(template_str: str, environment_variable_dict: dict = None):
+    """
+    This method update environment variable inside yaml file
+    :param dir_path:
+    :param yaml_file:
+    :param environment_variable_dict: add it for testing purpose
+    :return yaml text:
+    """
+    if not environment_variable_dict:
+        environment_variable_dict = environment_variables.environment_variables_dict
+    oc = OC(kubeadmin_password=environment_variable_dict.get('kubeadmin_password', ''))
+    # set prom token
+    environment_variable_dict['prom_token'] = oc.get_prom_token()
+    return Template(template_str).render(environment_variable_dict)
+
+
+def render_yaml_file_with_environment(dir_path: str, yaml_file: str, environment_variable_dict: dict = None):
+    """
+    This method update environment variable inside yaml file
+    :param dir_path:
+    :param yaml_file:
+    :param environment_variable_dict: add it for testing purpose
+    :return yaml text:
+    """
+    # Jinja render yaml file
+    with open(os.path.join(dir_path, yaml_file)) as f:
+        return render_yaml_string_with_environment(f.read(), environment_variable_dict)
+
+
 def update_environment_variable(dir_path: str, yaml_file: str, environment_variable_dict: dict = None):
     """
     This method update environment variable inside yaml file
@@ -13,16 +42,7 @@ def update_environment_variable(dir_path: str, yaml_file: str, environment_varia
     :param environment_variable_dict: add it for testing purpose
     :return:
     """
-    if not environment_variable_dict:
-        environment_variable_dict = environment_variables.environment_variables_dict
-    oc = OC(kubeadmin_password=environment_variable_dict.get('kubeadmin_password', ''))
-    # set prom token
-    environment_variable_dict['prom_token'] = oc.get_prom_token()
-    # Jinja render yaml file
-    with open(os.path.join(dir_path, yaml_file)) as f:
-        template_str = f.read()
-    tm = Template(template_str)
-    data = tm.render(environment_variable_dict)
+    data = render_yaml_file_with_environment(dir_path, yaml_file, environment_variable_dict)
     yaml_file = yaml_file.replace('_template', '')
     with open(os.path.join(dir_path, yaml_file), 'w') as f:
         f.write(data)
@@ -30,7 +50,7 @@ def update_environment_variable(dir_path: str, yaml_file: str, environment_varia
 
 def delete_generate_file(full_path_yaml: str):
     """
-    This method cleans up a generated file name
+    This method update environment variable inside yaml file
     :param full_path_yaml:
     :return:
     """
