04b4540fee8e61e3831747c31fb604e61e19ed0d
ebattat <ebattat@redhat.com>
2021-12-16 23:13:13 +0200

    adding fix for vm log capture

--- benchmark_runner/common/oc/oc.py
+++ benchmark_runner/common/oc/oc.py
@@ -317,7 +317,7 @@ class OC(SSH):
         """
         output_filename = f"{self.__run_artifacts}/{vm_name}"
         self.run(
-            cmd=f"virtctl console -n {environment_variables.environment_variables_dict['namespace']} {vm_name} > {output_filename}",
+            cmd=f"virtctl console -n {environment_variables.environment_variables_dict['namespace']} {vm_name} | tee -a {output_filename}",
             background=True)
 
     @logger_time_stamp
