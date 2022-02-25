0dbcfcc11bfe875d4794c41b3ad96d8e670ac7a4
ebattat <73884315+ebattat@users.noreply.github.com>
2022-02-25 16:51:35 +0200

    Update 01_registered_nightly_cnv_template.sh

--- benchmark_runner/common/ocp_resources/cnv/template/01_registered_nightly_cnv_template.sh
+++ benchmark_runner/common/ocp_resources/cnv/template/01_registered_nightly_cnv_template.sh
@@ -5,7 +5,7 @@ QUAY_AUTH=$(echo -n "${QUAY_USERNAME}:${QUAY_PASSWORD}" | base64 -w 0)
 podman login quay.io -u $QUAY_USERNAME -p $QUAY_PASSWORD
 QUAY_AUTH=$(cat /run/user/$(id -u)/containers/auth.json | jq -r '.auths["quay.io"].auth')
 jq --arg QUAY_AUTH "$QUAY_AUTH" '.auths += {"quay.io/openshift-cnv": {"auth":$QUAY_AUTH,"email":""}}' global-pull-secret.json > global-pull-secret.json.tmp
-mv global-pull-secret.json.tmp global-pull-secret.json
+mv -f global-pull-secret.json.tmp global-pull-secret.json
 oc set data secret/pull-secret -n openshift-config --from-file=.dockerconfigjson=global-pull-secret.json
-rm global-pull-secret.json
+rm -f global-pull-secret.json
 oc wait mcp master worker --for condition=updated --timeout=20m
