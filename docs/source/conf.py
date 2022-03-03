953222e5df6bd07fffcb965341492c5f1f7ef2f7
ebattat <73884315+ebattat@users.noreply.github.com>
2022-03-03 12:16:40 +0200

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -21,16 +21,16 @@ project = 'benchmark-runner'
 copyright = '2022, Eli Battat'
 author = 'Eli Battat'
 
-# The full version, including alpha/beta/rc tags
-release = '1.0.291'
-
-
 # -- General configuration ---------------------------------------------------
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
+    'sphinx.ext.napoleon',
+    'sphinx.ext.autodoc',
+    'sphinx.ext.viewcode',
+    'sphinx.ext.coverage',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -47,7 +47,7 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
