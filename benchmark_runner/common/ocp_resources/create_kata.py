9265e1ee76d607a4b624c7a9800911e891d7cf04
Robert Krawitz <rkrawitz@redhat.com>
2022-03-15 09:03:17 -0400

    Updates to the un-revert of the kata fix

--- benchmark_runner/common/ocp_resources/create_kata.py
+++ benchmark_runner/common/ocp_resources/create_kata.py
@@ -20,39 +20,40 @@ class CreateKata(CreateOCPResourceOperations):
         self.__path = path
         self.__resource_list = resource_list
 
+    def __install_and_wait_for_resource_with_retry(self, yaml_file: str, resource_type: str, resource: str):
+        """
+        Create a resource where the creation process itself may fail
+        without indicating an error and then have to be retried.
+        :param yaml_file:YAML file to create the resource
+        :param resource_type:type of resource to create
+        :param resource: name of resource to create
+        :return:
+        """
+        current_wait_time = 0
+        while current_wait_time < int(environment_variables.environment_variables_dict['timeout']):
+            self.__oc._create_async(yaml_file)
+            # We cannot wait for a condition here, because the
+            # create_async may simply not work even if it returns success.
+            time.sleep(OC.SLEEP_TIME)
+            if self.__oc.run(f'if oc get {resource_type} {resource} > /dev/null 2>&1 ; then echo succeeded; fi') == 'succeeded':
+                return True
+            current_wait_time += OC.SLEEP_TIME
+        return False
+
     @typechecked
     @logger_time_stamp
-    def create_kata(self, path: str, resource_list: list):
+    def create_kata(self):
         """
         This method create kata resource
         :param path:path of resource files
         :param resource_list: kata resource lists
         :return:
         """
-        def install_and_wait_for_resource(self, yaml_file: str, resource_type: str, resource: str):
-            """
-            Create a resource where the creation process itself may fail and has to be retried
-            :param yaml_file:YAML file to create the resource
-            :param resource_type:type of resource to create
-            :param resource: name of resource to create
-            :return:
-            """
-            current_wait_time = 0
-            while current_wait_time < int(environment_variables.environment_variables_dict['timeout']):
-                self._create_async(yaml_file)
-                # We cannot wait for a condition here, because the
-                # create_async may simply not work even if it returns success.
-                time.sleep(OC.SLEEP_TIME)
-                if self.run(f'if oc get {resource_type} {resource} > /dev/null 2>&1 ; then echo succeeded; fi') == 'succeeded':
-                    return True
-                current_wait_time += OC.SLEEP_TIME
-            return False
-
-        for resource in resource_list:
+        for resource in self.__resource_list:
             logger.info(f'run {resource}')
             if '01_operator.yaml' == resource:
                 # Wait for kataconfig CRD to exist
-                self._create_async(os.path.join(path, resource))
+                self.__oc._create_async(os.path.join(self.__path, resource))
                 self.wait_for_ocp_resource_create(resource='kata',
                                                   verify_cmd='if oc get crd kataconfigs.kataconfiguration.openshift.io >/dev/null 2>&1 ; then echo succeeded ; fi',
                                                   status='succeeded')
@@ -62,16 +63,16 @@ class CreateKata(CreateOCPResourceOperations):
                 # to apply it doesn't "take" unless some other things
                 # are already up.  So we have to keep applying the
                 # kataconfig until it's present.
-                if not install_and_wait_for_resource(self, os.path.join(path, resource), 'kataconfig', 'example-kataconfig'):
+                if not self.__install_and_wait_for_resource_with_retry(os.path.join(self.__path, resource), 'kataconfig', 'example-kataconfig'):
                     raise KataInstallationFailed('Failed to apply kataconfig resource')
                 # Next, we have to wait for the kata bits to actually install
                 self.wait_for_ocp_resource_create(resource='kata',
                                                   verify_cmd="oc get kataconfig -ojsonpath='{.items[0].status.installationStatus.IsInProgress}'",
                                                   status='false')
-                total_nodes_count = self.run(cmd="oc get kataconfig -ojsonpath='{.items[0].status.total_nodes_count}'")
-                completed_nodes_count = self.run(cmd="oc get kataconfig -ojsonpath='{.items[0].status.installationStatus.completed.completed_nodes_count}'")
+                total_nodes_count = self.__oc.run(cmd="oc get kataconfig -ojsonpath='{.items[0].status.total_nodes_count}'")
+                completed_nodes_count = self.__oc.run(cmd="oc get kataconfig -ojsonpath='{.items[0].status.installationStatus.completed.completed_nodes_count}'")
                 if total_nodes_count != completed_nodes_count:
                     raise KataInstallationFailed(f'not all nodes installed successfully total {total_nodes_count} != completed {completed_nodes_count}')
             elif '03_ocp48_patch.sh' == resource:
-                self.run(cmd=f'chmod +x {os.path.join(path, resource)}; {path}/./{resource}')
+                self.__oc.run(cmd=f'chmod +x {os.path.join(self.__path, resource)}; {os.path.join(self.__path, resource)}')
         return True
