redhat-performance/benchmark-runner

commit e10eb510d7cd2ba62c4f986ff7b854124949eca0

elasticsearch: allow SSL connection to the ElasticSearch server (#394)
Set `ELASTICSEARCH_URL_PROTOCOL=https`. Using 'http' by default.

Signed-off-by: Kevin Pouget <kpouget@redhat.com>

benchmark_runner/workloads/workloads_operations.py
@@ -46,6 +46,7 @@ class WorkloadsOperations:
         self._es_port = self._environment_variables_dict.get('elasticsearch_port', '')
         self._es_user = self._environment_variables_dict.get('elasticsearch_user', '')
         self._es_password = self._environment_variables_dict.get('elasticsearch_password', '')
+        self._es_url_protocol = self._environment_variables_dict['elasticsearch_url_protocol']
         self._timeout = int(self._environment_variables_dict.get('timeout', ''))
         # Elasticsearch connection
         if self._es_host and self._es_port:
@@ -53,6 +54,7 @@ class WorkloadsOperations:
                                                            es_port=self._es_port,
                                                            es_user=self._es_user,
                                                            es_password=self._es_password,
+                                                           es_url_protocol=self.es_url_protocol,
                                                            timeout=self._timeout)
         # Generate templates class
         self._template = TemplateOperations(workload=self._workload)

benchmark_runner/common/elasticsearch/elasticsearch_operations.py
@@ -1,7 +1,10 @@
 
 import time
+import ssl
+import urllib3
 from typeguard import typechecked
 from elasticsearch import Elasticsearch
+from elasticsearch.connection import create_ssl_context
 from elasticsearch_dsl import Search
 from datetime import datetime
 
@@ -22,20 +25,33 @@ class ElasticSearchOperations:
     MAX_SEARCH_RESULTS = 1000
     MIN_SEARCH_RESULTS = 100
 
-    def __init__(self, es_host: str, es_port: str, es_user: str = '', es_password: str = '', timeout: int = 2000):
+    def __init__(self, es_host: str, es_port: str, es_user: str = '', es_password: str = '', es_url_protocol: str = 'http', timeout: int = 2000):
         self.__es_host = es_host
         self.__es_port = es_port
         self.__es_user = es_user
         self.__es_password = es_password
+        self.__es_url_protocol = es_url_protocol
         self.__timeout = timeout
+
+        kwargs = {}
+        if self.__es_url_protocol == 'https':
+            ssl_context = create_ssl_context()
+            ssl_context.check_hostname = False
+            ssl_context.verify_mode = ssl.CERT_NONE
+
+            kwargs["ssl_context"] = ssl_context
+
         if self.__es_password:
-            self.__es_url = f"http://{self.__es_user}:{self.__es_password}@{self.__es_host}:{self.__es_port}"
-            self.__es = Elasticsearch([self.__es_url])
+            self.__es_url = f"{self.__es_url_protocol}://{self.__es_user}:{self.__es_password}@{self.__es_host}:{self.__es_port}"
+
+            host_arg = self.__es_url
         else:
+            host_arg = dict(host=self.__es_host)
             if self.__es_port:
-                self.__es = Elasticsearch([{'host': self.__es_host, 'port': self.__es_port}])
-            else:
-                self.__es = Elasticsearch([{'host': self.__es_host}])
+                host_arg['port'] = self.__es_port
+
+        self.__es = Elasticsearch([host_arg], **kwargs)
+
         self._hits = 0
 
     @property

benchmark_runner/main/environment_variables.py
@@ -36,6 +36,8 @@ class EnvironmentVariables:
         self._environment_variables_dict['elasticsearch_port'] = os.environ.get('ELASTICSEARCH_PORT', '')
         self._environment_variables_dict['elasticsearch_user'] = os.environ.get('ELASTICSEARCH_USER', '')
         self._environment_variables_dict['elasticsearch_password'] = os.environ.get('ELASTICSEARCH_PASSWORD', '')
+        # 'http'(Default) / 'https' to use SSL to connect ElasticSearch
+        self._environment_variables_dict['elasticsearch_url_protocol'] = os.environ.get('ELASTICSEARCH_URL_PROTOCOL', 'http')
 
         # Workaround for Kata CPU offline problem in 4.9/4.10
         # Set to True to
@@ -198,10 +200,10 @@ class EnvironmentVariables:
 
         # ElasticSearch url
         if self._environment_variables_dict.get('elasticsearch_password', ''):
-            self._environment_variables_dict['elasticsearch_url'] = f"http://{self._environment_variables_dict.get('elasticsearch_user', '')}:{self._environment_variables_dict.get('elasticsearch_password', '')}@{self._environment_variables_dict.get('elasticsearch', '')}:{self._environment_variables_dict.get('elasticsearch_port', '')}"
+            self._environment_variables_dict['elasticsearch_url'] = f"{self._environment_variables_dict['elasticsearch_url_protocol']}://{self._environment_variables_dict.get('elasticsearch_user', '')}:{self._environment_variables_dict.get('elasticsearch_password', '')}@{self._environment_variables_dict.get('elasticsearch', '')}:{self._environment_variables_dict.get('elasticsearch_port', '')}"
         else:
             if self._environment_variables_dict['elasticsearch'] and self._environment_variables_dict.get('elasticsearch_port', ''):
-                self._environment_variables_dict['elasticsearch_url'] = f"http://{self._environment_variables_dict.get('elasticsearch', '')}:{self._environment_variables_dict.get('elasticsearch_port', '')}"
+                self._environment_variables_dict['elasticsearch_url'] = f"{self._environment_variables_dict['elasticsearch_url_protocol']}://{self._environment_variables_dict.get('elasticsearch', '')}:{self._environment_variables_dict.get('elasticsearch_port', '')}"
             else:
                 self._environment_variables_dict['elasticsearch_url'] = ''
 

commit 708bd59b88e55ebc6991c58f4828e2e14445d31e

benchmark_runner/workloads/workloads_operations.py: fix typo introduced in #394

benchmark_runner/workloads/workloads_operations.py
@@ -54,7 +54,7 @@ class WorkloadsOperations:
                                                            es_port=self._es_port,
                                                            es_user=self._es_user,
                                                            es_password=self._es_password,
-                                                           es_url_protocol=self.es_url_protocol,
+                                                           es_url_protocol=self._es_url_protocol,
                                                            timeout=self._timeout)
         # Generate templates class
         self._template = TemplateOperations(workload=self._workload)