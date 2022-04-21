516a91ffc8b22ab8629b1d8d61241c5848afb10d
Robert Krawitz <rkrawitz@redhat.com>
2022-04-21 08:57:11 -0400

    Fix another typo

--- benchmark_runner/common/ocp_resources/migrate_infra.py
+++ benchmark_runner/common/ocp_resources/migrate_infra.py
@@ -10,15 +10,14 @@ class MigrateInfra(CreateOCPResourceOperations):
     """
     This class is create OCP resources
     """
-    def __init__(self, oc: OC, path: str, resource_list: list, ibm_blk_disk_name: list):
+    def __init__(self, oc: OC, path: str, resource_list: list):
         super().__init__(oc)
         self.__oc = oc
         self.__path = path
         self.__resource_list = resource_list
-        self.__ibm_blk_disk_name = ibm_blk_disk_name
 
     @logger_time_stamp
-    def create_odf(self):
+    def migrate_infra(self):
         """
         This method create odf
         :return:
@@ -27,9 +26,8 @@ class MigrateInfra(CreateOCPResourceOperations):
             logger.info(f'run {resource}')
             if resource == '01_cluster-monitoring-configmap-template.yaml':
                 nodes = self.__oc.get_master_nodes().split()
-                if len(nodes) < 1:
-                    pass
-                for node in nodes:
-                    self.run(f'oc label {node} node-role.kubernetes.io/infra=')
-                self.__oc._create_async(yaml=os.path.join(self.__path, resource))
+                if len(nodes) >= 1:
+                    for node in nodes:
+                        self.run(f'oc label {node} node-role.kubernetes.io/infra=')
+                    self.__oc._create_async(yaml=os.path.join(self.__path, resource))
         return True
