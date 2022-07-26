72c4a9f2006d52db5e4e6cb59b5980107a9b64ce
ebattat <73884315+ebattat@users.noreply.github.com>
2022-07-27 01:13:42 +0300

    add clusterbuster termination timeout (#456)

--- benchmark_runner/clusterbuster/clusterbuster_workloads.py
+++ benchmark_runner/clusterbuster/clusterbuster_workloads.py
@@ -91,7 +91,7 @@ class ClusterBusterWorkloads(WorkloadsOperations):
         This method run ClusterBuster workload
         :return:
         """
-        self.__ssh.run(cmd=f'{self.__clusterbuster_path} --run_type={self._run_type} --client-pin-node={self._pin_node1} --server-pin-node={self._pin_node2} --sync-pin-node={self._pin_node2} --basename={self.__namespace} --artifactdir={self.__artifactdir} --analyze={self.__result_report} > {self.__clusterbuster_log} 2>&1')
+        self.__ssh.run(cmd=f'{self.__clusterbuster_path} --run_type={self._run_type} --client-pin-node={self._pin_node1} --server-pin-node={self._pin_node2} --sync-pin-node={self._pin_node2} --force-cleanup-i-know-this-is-dangerous=600 --basename={self.__namespace} --artifactdir={self.__artifactdir} --analyze={self.__result_report} > {self.__clusterbuster_log} 2>&1')
         # Check that Result file exist and not empty
         if os.path.exists(os.path.join(self.__result_report)) and not os.stat(self.__result_report).st_size == 0:
             self.__ssh.run(cmd=f'cp {self.__result_report} {self._run_artifacts_path}')
