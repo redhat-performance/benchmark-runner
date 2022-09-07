f2fc72d16c044dc585c4ff35f09ed26f3ade69dc
ebattat <ebattat@redhat.com>
2022-09-07 20:15:32 +0300

    add Jetlag (assisted installer) support

--- benchmark_runner/main/main.py
+++ benchmark_runner/main/main.py
@@ -9,6 +9,7 @@ from benchmark_runner.main.environment_variables import environment_variables
 from benchmark_runner.common.clouds.Azure.azure_operations import AzureOperations
 from benchmark_runner.common.clouds.IBM.ibm_operations import IBMOperations
 from benchmark_runner.clusterbuster.clusterbuster_workloads import ClusterBusterWorkloads
+from benchmark_runner.common.oc.oc import OC
 
 # logger
 log_level = os.environ.get('log_level', 'INFO').upper()
@@ -32,6 +33,7 @@ def main():
     clusterbuster_workload = None
     environment_variables_dict = environment_variables.environment_variables_dict
     # environment variables data
+    provision_user = environment_variables_dict.get('provision_user', '')
     workload = environment_variables_dict.get('workload', '')
     azure_cluster_stop = environment_variables_dict.get('azure_cluster_stop', '')
     azure_cluster_start = environment_variables_dict.get('azure_cluster_start', '')
@@ -77,24 +79,24 @@ def main():
             logger.info(azure_operation.stop_vm(vm_name=azure_vm_name))
 
     @logger_time_stamp
-    def install_ocp(step: str):
+    def install_ocp(ibm_operations: IBMOperations, step: str):
         """
         This method install ocp version
         :return:
         """
-        ibm_operations = IBMOperations(user=environment_variables_dict.get('provision_user', ''))
-        ibm_operations.ibm_connect()
-        if step == 'run_ibm_ocp_ipi_installer':
+        if step == 'run_ibm_ocp_installer':
             ibm_operations.update_ocp_version()
-            ibm_operations.run_ibm_ocp_ipi_installer()
+            ibm_operations.run_ibm_ocp_installer()
         elif step == 'verify_install_complete':
             complete = ibm_operations.verify_install_complete()
             if complete:
-                logger.info(f'Installed OCP {install_ocp_version} successfully')
+                logger.info(f'OCP {install_ocp_version} installation complete successfully')
+                logger.info(f'restart pod ci')
+                ibm_operations.restart_pod_ci()
                 logger.info(f'Update GitHub OCP credentials')
                 ibm_operations.update_ocp_github_credentials()
             else:
-                logger.info(f'Failed to install OCP {install_ocp_version}')
+                logger.info(f'OCP {install_ocp_version} installation failed')
         ibm_operations.ibm_disconnect()
 
     @logger_time_stamp
@@ -103,16 +105,16 @@ def main():
         This method install ocp resources
         :return:
         """
+        ibm_operations = IBMOperations(user=provision_user)
+        ibm_operations.ibm_connect()
         install_resources_list = environment_variables_dict.get('install_resources_list', '')
         resources = ast.literal_eval(install_resources_list)
         logger.info(f'Start IBM OCP resources installation')
-        ibm_oc_operations = IBMOperations(user=environment_variables_dict.get('provision_oc_user', ''))
-        ibm_oc_operations.ibm_connect()
-        oc = ibm_oc_operations.oc_login()
-        ibm_oc_operations.verify_cluster_is_up(oc)
+        oc = ibm_operations.oc_login()
+        ibm_operations.verify_cluster_is_up(oc)
         # ibm_blk_disk_name for odf install
-        ibm_oc_operations.install_ocp_resources(resources=resources, ibm_blk_disk_name=ibm_oc_operations.get_ibm_disks_blk_name())
-        ibm_oc_operations.ibm_disconnect()
+        ibm_operations.install_ocp_resources(resources=resources, ibm_blk_disk_name=ibm_operations.get_ibm_disks_blk_name())
+        ibm_operations.ibm_disconnect()
         logger.info(f'End IBM OCP resources installation')
 
     @logger_time_stamp
@@ -164,8 +166,15 @@ def main():
         azure_cluster_start_stop()
     # install_ocp_version
     elif install_ocp_version:
+        ibm_operations = IBMOperations(user=provision_user)
+        ibm_operations.ibm_connect()
         install_step = environment_variables_dict.get('install_step', '')
-        install_ocp(step=install_step)
+        if not ibm_operations.version_already_installed() and install_step == 'run_ibm_ocp_installer':
+            install_ocp(ibm_operations=ibm_operations, step=install_step)
+        elif install_step == 'verify_install_complete':
+            install_ocp(ibm_operations=ibm_operations, step=install_step)
+        else:
+            logger.info(f'{ibm_operations.get_ocp_server_version().strip()} version already installed, skip OCP installation')
     # install_ocp_resource
     elif install_ocp_resources:
         install_resources()
