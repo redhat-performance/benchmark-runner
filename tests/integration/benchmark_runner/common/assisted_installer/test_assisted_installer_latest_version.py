8bd5b1a7c76af43c1cc7b853708988003f9b80c4
ebattat <ebattat@redhat.com>
2022-12-26 10:03:25 +0200

    add parsing of release candiate version

--- tests/integration/benchmark_runner/common/assisted_installer/test_assisted_installer_latest_version.py
+++ tests/integration/benchmark_runner/common/assisted_installer/test_assisted_installer_latest_version.py
@@ -7,8 +7,9 @@ def test_get_release_latest_version():
     This method returns the latest version
     @return:
     """
+    RELEASE_VERSION_LENGTH = 3
     assisted_installer_versions = AssistedInstallerVersions()
-    assert len(assisted_installer_versions.get_latest_version(latest_version='4.11').split('.')) == 3
+    assert len(assisted_installer_versions.get_latest_version(latest_version='4.11').split('.')) == RELEASE_VERSION_LENGTH
 
 
 def test_get_release_candidate_latest_version():
@@ -16,5 +17,6 @@ def test_get_release_candidate_latest_version():
     This method returns the latest version
     @return:
     """
+    RELEASE_CANDIDATE_VERSION_LENGTH = 4
     assisted_installer_versions = AssistedInstallerVersions()
-    assert len(assisted_installer_versions.get_latest_version(latest_version='4.11.0-rc').split('.')) == 4
+    assert len(assisted_installer_versions.get_latest_version(latest_version='4.11.0-rc').split('.')) == RELEASE_CANDIDATE_VERSION_LENGTH
