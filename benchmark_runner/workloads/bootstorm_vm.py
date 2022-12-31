1e0cba83c4b2a7fe7d99835e9a95cb968a4d6a80
ebattat <73884315+ebattat@users.noreply.github.com>
2022-12-31 15:59:46 +0200

    add wait for terminate bootstorm vm (#520)

--- benchmark_runner/workloads/bootstorm_vm.py
+++ benchmark_runner/workloads/bootstorm_vm.py
@@ -49,12 +49,18 @@ class BootstormVM(WorkloadsOperations):
             # verify that data upload to elastic search according to unique uuid
             self._verify_elasticsearch_data_uploaded(index=self.__es_index, uuid=self._uuid)
 
-    def __delete_vm_scale(self, vm_num: str):
+    def __delete_async_vm_scale(self, vm_num: str):
         """
-        This method deletes VMs in parallel
+        This method deletes VMs async in parallel
         """
         self._oc.delete_async(
             yaml=os.path.join(f'{self._run_artifacts_path}', f'{self.__name}_{vm_num}.yaml'))
+
+    def __wait_for_terminate_vm_scale(self, vm_num: str):
+        """
+        This method waits for VMs terminate in parallel
+        """
+        self._oc.wait_for_vm_terminate(vm_name=f'bootstorm-vm-{self._trunc_uuid}-{vm_num}')
 
     @logger_time_stamp
     def run(self):
@@ -94,7 +100,7 @@ class BootstormVM(WorkloadsOperations):
                 # create run bulks
                 bulks = tuple(self.split_run_bulks(iterable=range(self._scale * len(self._scale_node_list)), limit=self._threads_limit))
                 # create, run and delete vms
-                for target in (self.__create_vm_scale, self.__run_vm_scale, self.__delete_vm_scale):
+                for target in (self.__create_vm_scale, self.__run_vm_scale, self.__delete_async_vm_scale, self.__wait_for_terminate_vm_scale):
                     proc = []
                     for bulk in bulks:
                         for vm_num in bulk:
