9d6092516a480cd8fa41047aecca8c8b6844f780
ebattat <73884315+ebattat@users.noreply.github.com>
2023-01-01 22:39:51 +0200

    bootstom fix delete vm (#525)

--- benchmark_runner/workloads/bootstorm_vm.py
+++ benchmark_runner/workloads/bootstorm_vm.py
@@ -49,13 +49,11 @@ class BootstormVM(WorkloadsOperations):
             # verify that data upload to elastic search according to unique uuid
             self._verify_elasticsearch_data_uploaded(index=self.__es_index, uuid=self._uuid)
 
-    def __delete_vm_scale(self, vm_num: str):
+    def __delete_all_vms(self):
         """
-        This method deletes VMs in parallel
+        This method deletes all VMs
         """
-        self._oc.delete_vm_sync(
-            yaml=os.path.join(f'{self._run_artifacts_path}', f'{self.__name}_{vm_num}.yaml'),
-            vm_name=f'bootstorm-vm-{self._trunc_uuid}-{vm_num}')
+        self._oc.delete_all_vms()
 
     @logger_time_stamp
     def run(self):
@@ -95,7 +93,7 @@ class BootstormVM(WorkloadsOperations):
                 # create run bulks
                 bulks = tuple(self.split_run_bulks(iterable=range(self._scale * len(self._scale_node_list)), limit=self._threads_limit))
                 # create, run and delete vms
-                for target in (self.__create_vm_scale, self.__run_vm_scale, self.__delete_vm_scale):
+                for target in (self.__create_vm_scale, self.__run_vm_scale):
                     proc = []
                     for bulk in bulks:
                         for vm_num in bulk:
@@ -107,7 +105,8 @@ class BootstormVM(WorkloadsOperations):
                         # sleep between bulks
                         time.sleep(self._bulk_sleep_time)
                         proc = []
-                # delete namespace
+                # delete all vms and namespace
+                self.__delete_all_vms()
                 self._oc.delete_async(yaml=os.path.join(f'{self._run_artifacts_path}', 'namespace.yaml'))
         except ElasticSearchDataNotUploaded as err:
             self._oc.delete_vm_sync(
