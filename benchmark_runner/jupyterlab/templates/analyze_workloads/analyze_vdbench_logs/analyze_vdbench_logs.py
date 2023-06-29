40e0082f6a47a6288ca65a182780f8bf02377759
Eli Battat <ebattat@redhat.com>
2023-06-29 17:24:52 +0300

    compare_workload_artifacts

--- benchmark_runner/jupyterlab/templates/analyze_workloads/analyze_vdbench_logs/analyze_vdbench_logs.py
+++ benchmark_runner/jupyterlab/templates/analyze_workloads/analyze_vdbench_logs/analyze_vdbench_logs.py
@@ -8,7 +8,7 @@ from IPython.display import display
 # display bokeh
 from bokeh.plotting import figure, show
 from bokeh.io import output_notebook, output_file
-from bokeh.models import HoverTool
+from bokeh.models import HoverTool, Label, ColumnDataSource, LabelSet, Circle
 
 from typeguard import typechecked
 
@@ -217,3 +217,247 @@ class AnalyzeVdbenchLogs:
 
             # Display the graph
             show(p)
+
+    @staticmethod
+    def compare_results_latency(csv_log_files1: str, csv_log_files2: str, legend_label1: str, legend_label2: str):
+        """
+        This method compare between latency results (Resp column)
+        @param csv_log_files1:
+        @param csv_log_files2:
+        @param legend_label1: first bar legend
+        @param legend_label2: second bar legend
+        @return:
+        """
+        # Read the CSV file into a DataFrame
+        df1 = pd.read_csv(csv_log_files2[0])
+        df2 = pd.read_csv(csv_log_files1[0])
+
+        # Filter the DataFrame to include only the desired rows
+        df1 = df1[df1["Run"].isin(
+            ["oltp1", "oltp2", "oltphw", "odss2", "odss128", "4kb_read_16threads", "64kb_read_4threads",
+             "4kb_write_16threads", "64kb_write_4threads"])]
+        df2 = df2[df2["Run"].isin(
+            ["oltp1", "oltp2", "oltphw", "odss2", "odss128", "4kb_read_16threads", "64kb_read_4threads",
+             "4kb_write_16threads", "64kb_write_4threads"])]
+
+        # Set the index to the "Run" column
+        df1.set_index("Run", inplace=True)
+        df2.set_index("Run", inplace=True)
+
+        # Convert the DataFrame to a dictionary
+        result1 = df1["Resp"].to_dict()
+        result2 = df2["Resp"].to_dict()
+        # Extract the keys and values from the dictionaries
+        keys = list(result1.keys())
+        values1 = list(result1.values())
+        values2 = list(result2.values())
+
+        # Calculate the x-coordinates for data1 and results bars
+        x1 = [i - 0.2 for i in range(len(keys))]
+        x2 = [i + 0.2 for i in range(len(keys))]
+
+        # Shift the x-coordinates to the right
+        x1_shifted = [x + 0.5 for x in x1]
+        x2_shifted = [x + 0.5 for x in x2]
+
+        # Create a Bokeh figure
+        p = figure(title="Vdbench Latency Comparison - Lower better", x_range=keys, y_axis_label="Latency (sec)",
+                   width=1200)
+
+        # Add vertical bars for data1
+        r1 = p.vbar(x=x1_shifted, top=values1, width=0.4, color='green', legend_label=legend_label2)
+
+        # Add vertical bars for data2
+        r2 = p.vbar(x=x2_shifted, top=values2, width=0.4, color='blue', legend_label=legend_label1)
+
+        # Customize the x-axis tick labels
+        p.xaxis.major_label_orientation = 45  # Rotate the labels by 45 degrees
+        p.xaxis.axis_label = "Applications"  # Add x-axis title
+        p.xaxis.major_label_text_font_size = "12pt"  # Increase the label font size
+
+        # Add tooltips for data1 bars
+        hover1 = HoverTool(renderers=[r1], tooltips=[(legend_label2, "@top{0.0000}")])
+        p.add_tools(hover1)
+
+        # Add tooltips for data2 bars
+        hover2 = HoverTool(renderers=[r2], tooltips=[(legend_label1, "@top{0.0000}")])
+        p.add_tools(hover2)
+
+        # Add labels for result1 bars
+        for i, value in enumerate(values1):
+            label = Label(x=x1_shifted[i], y=value, text=f"{value:.4f}", text_font_size="10pt", text_color="black",
+                          text_baseline="bottom", text_align="center")
+            p.add_layout(label)
+
+        # Add labels for result2 bars
+        for i, value in enumerate(values2):
+            label = Label(x=x2_shifted[i], y=value, text=f"{value:.4f}", text_font_size="10pt", text_color="black",
+                          text_baseline="bottom", text_align="center")
+            p.add_layout(label)
+
+        # Show the legend on the right side
+        p.legend.location = "top_left"
+
+        # Display the graph
+        output_notebook()
+        show(p)
+
+    @staticmethod
+    def compare_results(csv_log_files1: str, csv_log_files2: str, legend_label1: str, legend_label2: str,
+                        metric: str):
+        """
+        The method compare between log files results
+        @param csv_log_files1:
+        @param csv_log_files2:
+        @param legend_label1:
+        @param legend_label2:
+        @param metric: metric to compare can be 'IOPS' or 'Latency'
+        @return:
+        """
+        if metric == 'IOPS':
+            metric_key = 'Rate'
+            title = 'IOPS - higher is better'
+        elif metric == 'Latency':
+            metric_key = 'Resp'
+            title = 'Latency - lower is better'
+        else:  # default IOPS
+            metric_key = 'Rate'
+            title = 'IOPS - higher is better'
+
+            # Read the CSV file into a DataFrame
+        df1 = pd.read_csv(csv_log_files2[0])
+        df2 = pd.read_csv(csv_log_files1[0])
+
+        # Filter the DataFrame to include only the desired rows
+        columns = ["oltp1", "oltp2", "oltphw", "odss2", "odss128", "4kb_read_16threads", "64kb_read_4threads",
+                   "4kb_write_16threads", "64kb_write_4threads"]
+        df1 = df1[df1["Run"].isin(columns)]
+        df2 = df2[df2["Run"].isin(columns)]
+
+        # Set the index to the "Run" column
+        df1.set_index("Run", inplace=True)
+        df2.set_index("Run", inplace=True)
+
+        # Convert the DataFrame to a dictionary Rate/Resp
+        result1 = df1[metric_key].to_dict()
+        result2 = df2[metric_key].to_dict()
+
+        # Extract the keys and values from the dictionaries
+        keys = list(result1.keys())
+        values1 = list(result1.values())
+        values2 = list(result2.values())
+
+        # Calculate the x-coordinates for data1 and data2 bars
+        x1 = [i - 0.2 for i in range(len(keys))]
+        x2 = [i + 0.2 for i in range(len(keys))]
+
+        # Shift the x-coordinates to the right
+        x1_shifted = [x + 0.5 for x in x1]
+        x2_shifted = [x + 0.5 for x in x2]
+
+        # Create a Bokeh figure
+        p = figure(title=f"Vdbench {title}", x_range=keys, y_axis_label="Latency (sec)", width=1200, height=600)
+
+        # Adjust the y-axis range
+        p.y_range.start = 0
+        p.y_range.end = max(max(result1.values()), max(result2.values())) * 2
+
+        # Add vertical bars for data1
+        r1 = p.vbar(x=x1_shifted, top=values1, width=0.4, color='green', legend_label=legend_label2)
+
+        # Add vertical bars for data2
+        r2 = p.vbar(x=x2_shifted, top=values2, width=0.4, color='blue', legend_label=legend_label1)
+
+        # Percentage line
+        # Calculate the percentage difference between data1 and data2
+        percentage_diff = [(v1 - v2) / v2 * 100 if v2 != 0 else 0 for v1, v2 in zip(values1, values2)]
+        percentage_diff = [round(diff, 2) for diff in percentage_diff]  # Round to 2 decimal places
+
+        # Customize the x-axis tick labels
+        p.xaxis.major_label_orientation = 45  # Rotate the labels by 45 degrees
+        p.xaxis.axis_label = "Applications"  # Add x-axis title
+        p.xaxis.major_label_text_font_size = "12pt"  # Increase the label font size
+
+        # Add tooltips for data1 bars
+        hover1 = HoverTool(renderers=[r1], tooltips=[(legend_label2, "@top{0.00}")])
+        p.add_tools(hover1)
+
+        # Add tooltips for data2 bars
+        hover2 = HoverTool(renderers=[r2], tooltips=[(legend_label1, "@top{0.00}")])
+        p.add_tools(hover2)
+
+        # Add labels for result1 bars
+        for i, value in enumerate(values1):
+            label = Label(x=x1_shifted[i], y=value, text=f"{value:.2f}", text_font_size="10pt", text_color="black",
+                          text_baseline="bottom", text_align="center")
+            p.add_layout(label)
+
+        # Add labels for result2 bars
+        for i, value in enumerate(values2):
+            label = Label(x=x2_shifted[i], y=value, text=f"{value:.2f}", text_font_size="10pt", text_color="black",
+                          text_baseline="bottom", text_align="center")
+            p.add_layout(label)
+
+        # Calculate the maximum value from result1 and result2 dictionaries
+        min_value = min(min(values1), min(values2))
+
+        # Calculate the percentage difference between data1 and data2
+        if metric == 'Latency':
+            percentage_diff = [(v1 - v2) / v2 * 100 if v2 != 0 else 0 for v1, v2 in zip(values1, values2)]
+        elif metric == 'IOPS':
+            percentage_diff = [(v2 - v1) / v1 * 100 if v2 != 0 else 0 for v1, v2 in zip(values1, values2)]
+        percentage_diff = [round(diff, 2) for diff in percentage_diff]  # Round to 2 decimal places
+
+        # Calculate the x-coordinates for the dots
+        x_middle = [(x1_shifted[i] + x2_shifted[i]) / 2 for i in range(len(keys))]
+
+        # Calculate the y-coordinate for the dots
+        y_middle = [max(values1[i], values2[i]) + min_value for i in range(len(keys))]
+
+        # Add dots for each number on the line
+        dot_source = ColumnDataSource(data=dict(x=x_middle, y=y_middle, values=percentage_diff))
+        dot_glyph = Circle(x='x', y='y', size=6, line_color='red', fill_color='white')
+        p.add_glyph(dot_source, dot_glyph)
+
+        # Add lines showing the percentage difference
+        line_source = ColumnDataSource(data=dict(x=x_middle, y=y_middle, values=percentage_diff))
+        line_glyph = p.line(x='x', y='y', source=line_source, line_width=2, line_color="orange")
+
+        # Add labels for percentage difference
+        for i, diff in enumerate(percentage_diff):
+            x_pos = x_middle[i]
+            y_pos = y_middle[i] + 0.1  # Adjust the y-coordinate to move the labels above the line
+            label = Label(x=x_pos, y=y_pos, text=f"{diff}%", text_font_size="10pt", text_color="orange",
+                          text_baseline="bottom", text_align='center')  # Set text_color to 'green'
+            p.add_layout(label)
+
+        # Show the legend on the top-left corner
+        p.legend.location = "top_right"
+
+        # Display the graph
+        output_notebook()
+        show(p)
+
+    def compare_latency(self, csv_log_files1: str, csv_log_files2: str, legend_label1: str, legend_label2: str,
+                        metric: str = 'Latency'):
+        """
+        This method compare latency results between logs
+        @param csv_log_files2:
+        @param legend_label1:
+        @param legend_label2:
+        @param metric:
+        @return: default Latency
+        """
+        self.compare_results(csv_log_files1, csv_log_files2, legend_label1, legend_label2, metric=metric)
+
+    def compare_iops(self, csv_log_files1: str, csv_log_files2: str, legend_label1: str, legend_label2: str,
+                        metric: str = 'IOPS'):
+        """
+        This method compare iops results between logs
+        @param csv_log_files2:
+        @param legend_label1:
+        @param legend_label2:
+        @param metric: default IOPS
+        @return:
+        """
+        self.compare_results(csv_log_files1, csv_log_files2, legend_label1, legend_label2, metric=metric)
