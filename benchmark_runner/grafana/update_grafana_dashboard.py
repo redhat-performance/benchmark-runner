e1c1135a799a60f4a69a0565e3b71e998b1d6e62
ebattat <73884315+ebattat@users.noreply.github.com>
2023-09-13 11:06:30 +0300

    update grafana dashboard into folder (#655)

--- benchmark_runner/grafana/update_grafana_dashboard.py
+++ benchmark_runner/grafana/update_grafana_dashboard.py
@@ -6,7 +6,8 @@ from benchmark_runner.main.environment_variables import environment_variables
 environment_variables_dict = environment_variables.environment_variables_dict
 grafana_operations = GrafanaOperations(grafana_url=environment_variables_dict.get('grafana_url', ''),
                                        grafana_api_key=environment_variables_dict.get('grafana_api_key', ''),
-                                       grafana_json_path=environment_variables_dict.get('grafana_json_path', ''))
+                                       grafana_json_path=environment_variables_dict.get('grafana_json_path', ''),
+                                       grafana_folder_name=environment_variables_dict.get('grafana_folder_name', ''))
 # for debug: grafana_operations.fetch_all_dashboards()
 grafana_operations.read_dashboard_json()
 grafana_operations.increment_dashboard_version()
