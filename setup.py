b0a1d05a4af418f842437a9c1ab785e06652ef37
ebattat <ebattat@redhat.com>
2024-06-23 11:05:00 +0000

    bump version to exist version v1.0.617

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from os import path
 from setuptools import setup, find_packages
 
 
-__version__ = '1.0.617'  # update also .bumpversion.cfg
+__version__ = '1.0.618'  # update also .bumpversion.cfg
 
 
 here = path.abspath(path.dirname(__file__))
