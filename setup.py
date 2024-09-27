133eb530af58c0037fb363ecb089d7e514464779
Eli Battat <ebattat@redhat.com>
2024-09-27 16:56:27 +0300

    downgrade paramiko

--- setup.py
+++ setup.py
@@ -60,7 +60,7 @@ setup(
         'openshift-client==1.0.17',  # clusterbuster && prometheus metrics
         'prometheus-api-client==0.5.1',  # clusterbuster && prometheus metrics
         'pandas',  # required latest
-        'paramiko==3.5.0',
+        'paramiko==3.4.0',
         'PyGitHub==1.55',  # update secrets
         'PyYAML==6.0.1',
         'setuptools',  # for python3.12
