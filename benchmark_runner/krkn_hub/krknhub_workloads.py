ba93f14c22c20224ca311feb2eced3775aff48f9
Eli Battat <ebattat@redhat.com>
2025-03-16 10:23:59 +0200

    Fix krknhub workload

--- benchmark_runner/krkn_hub/krknhub_workloads.py
+++ benchmark_runner/krkn_hub/krknhub_workloads.py
@@ -53,7 +53,7 @@ class KrknHubWorkloads(WorkloadsOperations):
         """
         logger.info(f'run krkn-hub: {self.__krknhub_workload}')
         pod_log_path = f'{self._run_artifacts_path}/{self.__krknhub_workload}_pod.log'
-        workload_command = f'{self.__krknhub_environment_variables}; podman run --name={self.__krknhub_pod_name} --net=host --env-host=true -v /root/.kube/config:/home/krkn/.kube/config:Z quay.io/krkn-chaos/krkn-hub:{self.__krknhub_workload} > {pod_log_path}'
+        workload_command = f'{self.__krknhub_environment_variables}; podman run --rm --name={self.__krknhub_pod_name} --net=host --env-host=true -v /root/.kube/config:/home/krkn/.kube/config:Z quay.io/krkn-chaos/krkn-hub:{self.__krknhub_workload} > {pod_log_path}'
         logger.info(workload_command)
         self.__ssh.run(workload_command)
 
