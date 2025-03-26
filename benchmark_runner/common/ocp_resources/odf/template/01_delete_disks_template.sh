5acf954633c4f35253d19aa62686fd51e2c296e5
Eli Battat <ebattat@redhat.com>
2025-03-26 15:50:29 +0200

    ODF storage cleanup

--- benchmark_runner/common/ocp_resources/odf/template/01_delete_disks_template.sh
+++ benchmark_runner/common/ocp_resources/odf/template/01_delete_disks_template.sh
@@ -1,8 +1,14 @@
 #!/usr/bin/env sh
 
-while (($# >= 2)) ; do
+while (($# >= 2)); do
     node="$1"
     delete_disk_command="$2"
-    oc debug "node/$node"  -- chroot /host sh -c "$delete_disk_command"
+
+    echo "Executing deletion on node: $node"
+
+    set -x  # Enable debug mode to print the command being executed
+    oc debug "node/$node" -- chroot /host sh -c "$delete_disk_command"
+    set +x  # Disable debug mode
+
     shift 2
 done
