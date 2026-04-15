184d6436d0c023d1cfe681d4e5fb8bd3397eac61
Elib <ebattat@redhat.com>
2026-04-15 13:47:10 +0300

    fix prerecommit issues (#1191)

--- generate_golden_files.sh
+++ generate_golden_files.sh
@@ -1,8 +1,13 @@
 #!/usr/bin/env sh
 set -e
 
-python3 -m venv .venv
-. .venv/bin/activate
+if [ ! -d "venv" ]; then
+  python3 -m venv venv
+  . venv/bin/activate
+  pip install -e .
+else
+  . venv/bin/activate
+fi
 
 PYTHONPATH=. python3 \
   tests/unittest/benchmark_runner/common/template_operations/generate_golden_files.py
